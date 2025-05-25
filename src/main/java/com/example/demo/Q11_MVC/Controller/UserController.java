1db77a1d3e702ba71cf912786aebfb6ceae9de47
mrhkluck <mrhkluck@gmail.com>
2025-05-25 20:16:55 +0000

    Mvc User push

---
+++ src/main/java/com/example/demo/Q11_MVC/Controller/UserController.java
@@ -0,0 +1,52 @@
+package com.example.demo.Q11_MVC.Controller;
+import java.util.List;
+import Model.User;
+import Service.UserService;
+import View.UserView;
+
+public class UserController {
+    private UserService service;
+    private UserView view;
+
+    public UserController(UserService service, UserView view) {
+        this.service = service;
+        this.view = view;
+    }
+
+    // end point
+    // create user
+
+    public void createUser(String id, String name, String email, int number) {
+        service.addUser(new User(id, name, email, number));
+        view.displayMsg("User add successfully");
+    }
+
+    // read user
+    public void readUser(String id) {
+        // service.getById(id);
+        view.displayUser(service.getById(id));
+    }
+
+    public void readAllUser() {
+        List<User> rau = service.getAllUser();
+        if (rau.isEmpty()) {
+            view.displayMsg("koi bhi user nhi list khalli hai bsdk");
+        } else {
+            for (User u : rau) {
+                view.displayUser(u);
+            }
+        }
+    }
+
+    // Update
+    public void updateUser(String id, String newName, String newEmail,int number) {
+        boolean success = service.updateUser(id, newName, newEmail, number);
+        view.displayMsg(success ? "User updated successfully." : "User not found.");
+    }
+
+    // delete
+    public void deleteUser(String id) {
+        boolean success = service.deleteUser(id);
+        view.displayMsg(success ? "User deleted successfully." : "User not found.");
+    }
+}
---
+++ src/main/java/com/example/demo/Q11_MVC/Model/User.java
@@ -0,0 +1,48 @@
+package com.example.demo.Q11_MVC.Model;
+
+public class User {
+    private String id;
+    private String name;
+    private String email;
+    private int mobile_no;
+    //getter and setter
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public int getMobile_no() {
+        return mobile_no;
+    }
+    public void setMobile_no(int mobile_no) {
+        this.mobile_no = mobile_no;
+    }
+
+    //constructer
+    public User(){};
+    public User(String id, String name, String email, int mobile_no) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.mobile_no = mobile_no;
+    }
+
+     // data store formate
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", name=" + name + ", email=" + email + ", mobile_no=" + mobile_no + "]";
+    }
+}
